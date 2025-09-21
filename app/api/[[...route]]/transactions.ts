e638716a61717fb8b603c99efa5560711e8fd8e9
tanishq-khandelwal <tanishqkhandelwal2019@gmail.com>
2025-09-21 20:49:48 +0530

    chore: added husky setup

--- app/api/[[...route]]/transactions.ts
+++ app/api/[[...route]]/transactions.ts
@@ -1,47 +1,91 @@
 import { Hono } from "hono";
 import { z } from "zod";
 import { db } from "@/db/drizzle";
-import { categories, insertCategoriesSchema } from "@/db/schema";
-import { and, eq, inArray } from "drizzle-orm";
+import {
+  transactions,
+  insertTransactionsSchema,
+  categories,
+  accounts,
+} from "@/db/schema";
+import { parse, subDays } from "date-fns";
+import { and, eq, gte, inArray, lte, desc } from "drizzle-orm";
 import { clerkMiddleware, getAuth } from "@hono/clerk-auth";
 import { zValidator } from "@hono/zod-validator";
 import { createId } from "@paralleldrive/cuid2";
 const app = new Hono()
-  .get("/", async (c) => {
-    const auth = getAuth(c);
+  .get(
+    "/",
+    zValidator(
+      "query",
+      z.object({
+        from: z.string().optional(),
+        to: z.string().optional(),
+        accoundId: z.string().optional(),
+      }),
+    ),
+    clerkMiddleware(),
+    async (c) => {
+      const auth = getAuth(c);
+      const { from, to, accoundId } = c.req.valid("query");
 
-    if (!auth?.userId) {
-      return c.json({ error: "Unauthorized" }, 401);
-    }
+      if (!auth?.userId) {
+        return c.json({ error: "Unauthorized" }, 401);
+      }
 
-    try {
-      const data = await db
-        .select({
-          id: categories.id,
-          name: categories.name,
-        })
-        .from(categories)
-        .where(eq(categories.userId, auth.userId));
+      const defaultTo = new Date();
+      const defaultFrom = subDays(defaultTo, 30);
 
-      return c.json({ data });
-    } catch (error) {
-      console.error("Failed to fetch categories:", error);
-      return c.json(
-        {
-          error: "Failed to fetch data",
-          details: error instanceof Error ? error.message : "Unknown error",
-        },
-        500
-      );
-    }
-  })
+      const startDate = from
+        ? parse(from, "yyyy-MM-dd", new Date())
+        : defaultFrom;
+
+      const endDate = to ? parse(to, "yyyy-MM-dd", new Date()) : defaultTo;
+
+      try {
+        const data = await db
+          .select({
+            id: transactions.id,
+            category: categories.name,
+            categoryId: transactions.categoryId,
+            date: transactions.date,
+            payee: transactions.payee,
+            amount: transactions.amount,
+            account: accounts.name,
+            accountId: transactions.accountId,
+          })
+          .from(transactions)
+          .innerJoin(accounts, eq(transactions.accountId, accounts.id))
+          .leftJoin(categories, eq(transactions.categoryId, categories.id))
+          .where(
+            and(
+              accoundId ? eq(transactions.accountId, accoundId) : undefined,
+              eq(accounts.userId, auth.userId),
+              gte(transactions.date, startDate),
+              lte(transactions.date, endDate),
+            ),
+          )
+          .orderBy(desc(transactions.date));
+
+        return c.json({ data });
+      } catch (error) {
+        console.error("Failed to fetch categories:", error);
+        return c.json(
+          {
+            error: "Failed to fetch data",
+            details: error instanceof Error ? error.message : "Unknown error",
+          },
+          500,
+        );
+      }
+    },
+  )
   .get(
     "/:id",
     zValidator(
       "param",
       z.object({
         id: z.string().optional(),
-      })
+      }),
     ),
     clerkMiddleware(),
     async (c) => {
@@ -69,7 +113,7 @@ const app = new Hono()
       }
 
       return c.json({ data });
-    }
+    },
   )
   .post(
     "/",
@@ -78,7 +122,7 @@ const app = new Hono()
       "json",
       insertCategoriesSchema.pick({
         name: true,
-      })
+      }),
     ),
     async (c) => {
       const auth = getAuth(c);
@@ -99,7 +143,7 @@ const app = new Hono()
       return c.json({
         data,
       });
-    }
+    },
   )
   .post(
     "/bulk-delete",
@@ -108,7 +152,7 @@ const app = new Hono()
       "json",
       z.object({
         ids: z.array(z.string()),
-      })
+      }),
     ),
 
     async (c) => {
@@ -123,15 +167,15 @@ const app = new Hono()
         .where(
           and(
             eq(categories.userId, auth.userId),
-            inArray(categories.id, values.ids)
-          )
+            inArray(categories.id, values.ids),
+          ),
         )
         .returning({
           id: categories.id,
         });
 
       return c.json({ data });
-    }
+    },
   )
   .patch(
     "/:id",
@@ -140,13 +184,13 @@ const app = new Hono()
       "param",
       z.object({
         id: z.string().optional(),
-      })
+      }),
     ),
     zValidator(
       "json",
       insertCategoriesSchema.pick({
         name: true,
-      })
+      }),
     ),
     async (c) => {
       const auth = getAuth(c);
@@ -169,7 +213,7 @@ const app = new Hono()
         return c.json({ error: "Account not found" }, 404);
       }
       return c.json({ data });
-    }
+    },
   )
   .delete(
     "/:id",
@@ -178,7 +222,7 @@ const app = new Hono()
       "param",
       z.object({
         id: z.string().optional(),
-      })
+      }),
     ),
     async (c) => {
       const auth = getAuth(c);
@@ -201,7 +245,7 @@ const app = new Hono()
       }
 
       return c.json({ data });
-    }
+    },
   );
 
 export default app;
